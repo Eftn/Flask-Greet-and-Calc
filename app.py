c1ec031c21e0c0311382787480f2a16de42ea9a7
Eftn <65512069+Eftn@users.noreply.github.com>
2020-11-12 21:02:13 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,46 +1,16 @@
 from flask import Flask, request
-from operations import add, sub, mult, div
-
 app = Flask(__name__)
 
-@app.route("/add")
-@app.route("/math/add")
-def add1():
-#add A and B
-
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    total = add(a,b)
-    return str(total)
-
-@app.route("/sub")
-@app.route("/math/sub")
-def sub1():
-#add A and B
-
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    total = sub(a,b)
-    return str(total)
-
-@app.route("/mult")
-@app.route("/math/mult")
-def mult1():
-#add A and B
-
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    total = mult(a,b)
-    return str(total)
-
-@app.route("/div")
-@app.route("/math/div")
-def div1():
-#add A and B
-
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    total = div(a,b)
-    return str(total)
+@app.route('/welcome')
+def home():
+    return "Welcome "
 
 
+
+@app.route("/welcome/home")
+def home2():
+    return "Welcome home"
+
+@app.route("/welcome/back")
+def home3():
+    return "Welcome back"
